c24855b8a9d838cc52f1830894be24ecc3fdfbe7
studioplace <webstudioplace.com>
2016-08-04 17:33:53 -0400

    Project Upload

---
+++ button.js
@@ -0,0 +1,17 @@
+// Defining Angular App //
+
+(function(){
+
+    var app = angular.module('myButton',[]);
+
+    app.controller('ButtonController',['$scope', function($scope){
+
+    // Angular code goes here//
+
+    
+
+    }]);
+
+
+
+})();
---
+++ javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+
+
+$("button").addClass("animated shake");
+
+$("h1").addClass("text-primary");
+$("h5").css("color","green");
+
+
+
+// below is the end of jquery
+});
